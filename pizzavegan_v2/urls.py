f45d7d6d83755cbc1885f9b499fc5298c5c232a6
Cory Coward <cory@c2softwaresolutions.com>
2020-10-14 19:48:31 -0500

    Set up accounts templates

--- pizzavegan_v2/urls.py
+++ pizzavegan_v2/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
